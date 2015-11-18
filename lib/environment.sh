6fed4a662514c0968fbb5bb4405c47741d971454
edieguez <dieguezemmanuel@gmail.com>
2015-11-17 23:22:33 -0600

    Changes in the path variable

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,8 +1,10 @@
-GRAILS_HOME='/opt/grails-3.0.8'
-PATH+=":$GRAILS_HOME/bin"
-PATH+=':/opt/groovy-2.4.5/bin'
-PATH+=":/opt/gradle-2.8/bin"
-PATH+=":/opt/node-v4.2.1-linux-x64/bin"
-
 # Current directory
-PATH+=":."
+PATH=".:$PATH"
+
+# Groovy and Grails
+PATH+=':/opt/groovy/bin'
+GRAILS_HOME='/opt/grails'
+PATH+=":$GRAILS_HOME/bin"
+
+PATH+=":/opt/gradle/bin"
+PATH+=":/opt/nodejs/bin"
