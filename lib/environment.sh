ced85c6bcc11c188fdc0867becdd343b5c3c205e
edieguez <dieguezemmanuel@gmail.com>
2016-08-10 23:35:30 -0500

    Modified environment variables

--- lib/environment.sh
+++ lib/environment.sh
@@ -3,9 +3,10 @@ PATH=".:$PATH"
 
 # Groovy and Grails
 # export JAVA_HOME='/usr/lib/jvm/java-7-openjdk-amd64/'
-PATH+=':/opt/groovy/bin'
-GRAILS_HOME='/opt/grails'
-PATH+=":$GRAILS_HOME/bin"
+# PATH+=':/opt/groovy/bin'
+# GRAILS_HOME='/opt/grails'
+# PATH+=":$GRAILS_HOME/bin"
 
-PATH+=":/opt/gradle/bin"
-PATH+=":/opt/nodejs/bin"
+# PATH+=":/opt/gradle/bin"
+# PATH+=":/opt/nodejs/bin"
+# PATH+=":/opt/mongo/bin"
