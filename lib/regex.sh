1f98ae978141ca5249d34520279f54ca6c1d1d1f
edieguez <dieguezemmanuel@gmail.com>
2017-06-17 12:13:05 -0500

    Path issues
    Added a missing function in regex.sh lib
    Fixed a path for load the environment.sh script

--- lib/regex.sh
+++ lib/regex.sh
@@ -17,6 +17,21 @@
 #      REVISION:  ---
 #===============================================================================
 
+ F_BLACK=30;  F_RED=31;   F_GREEN=32;  F_YELLOW=33; F_BLUE=34;
+F_PURPLE=35;  F_CYAN=36;  F_GRAY=37;   F_WHITE=38;
+
+# Background Colors ------------------------------------------------------------
+ B_BLACK=40;  B_RED=41;   B_GREEN=42;  B_YELLOW=43; B_BLUE=44;
+B_PURPLE=45;  B_CYAN=46;  B_GRAY=47;
+
+# Formats ----------------------------------------------------------------------
+BOLD=01;  ITALIC=03  UNDER=04;  BLINK=05;  REVERSE=07;
+
+cout() {
+    format=$1; shift
+    echo -en "[${format}m""$*""[0m"
+}
+
 init_regex() {
     cout "$BOLD;$F_GREEN" "    init_regex... "
 
