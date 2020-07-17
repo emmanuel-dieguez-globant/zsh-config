974f36678c2efa43aa13366bf3297f16b2890adc
Emmanuel Diéguez <dieguezemmanuel@gmail.com>
2020-07-16 21:21:01 -0500

    Removed Maven env variable

--- system/environment.sh
+++ system/environment.sh
@@ -3,6 +3,5 @@ CUSTOM_PATH+="."
 
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/current'
 CUSTOM_PATH+=":$JAVA_HOME/bin"
-CUSTOM_PATH+=":/usr/local/opt/maven/bin"
 
 PATH="$CUSTOM_PATH:$PATH"
