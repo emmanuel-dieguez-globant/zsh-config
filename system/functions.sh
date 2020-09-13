90c617881c4b5d0c272e0dff1cb055cbb8a744ff
Emmanuel Diéguez <dieguezemmanuel@gmail.com>
2020-09-13 11:46:19 -0500

    Small changes to jdk plugin

--- system/functions.sh
+++ system/functions.sh
@@ -1,11 +1,15 @@
+activate() {
+    source ~/.virtualenvs/$1/bin/activate
+}
+
+jdk() {
+  ln -sfn "$JDK_HOME/$1" $HOME/.current_jdk
+}
+
 md() {
     mkdir -vp "$*" && cd "$*"
 }
 
 numcat() {
     sed '/./=' $1 | sed '/./N;s/\n/ /'
-}
-
-activate() {
-    source ~/.virtualenvs/$1/bin/activate
 }
