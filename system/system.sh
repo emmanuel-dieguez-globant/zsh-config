46e111914c42ad05bf267653907b39902ce3ee69
Emmanuel Diéguez <dieguezemmanuel@gmail.com>
2020-10-11 14:05:38 -0500

    Fixed small issue with the path variable

--- system/system.sh
+++ system/system.sh
@@ -2,9 +2,11 @@
 # Source functions
 #-------------------------------------------------------------------------------
 export ROOT_DIR="$HOME/.zsh-config"
-export PATH=".:$ROOT_DIR/bin:$PATH"
 
 source $ROOT_DIR/system/environment.sh
 source $ROOT_DIR/system/bundles.sh
 source $ROOT_DIR/system/aliases.sh
 source $ROOT_DIR/system/functions.sh
+
+# Insert current directory into PATH variable
+export PATH=".:$ROOT_DIR/bin$PATH"
