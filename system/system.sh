0e95bf81c1146db0bf2393e8b4c08a7c4e20c0bd
Emmanuel Diéguez <dieguezemmanuel@gmail.com>
2020-11-13 23:33:01 -0600

    Fixed error in path

--- system/system.sh
+++ system/system.sh
@@ -3,10 +3,10 @@
 #-------------------------------------------------------------------------------
 export ROOT_DIR="$HOME/.zsh-config"
 
+# Insert current directory into PATH variable
+export CUSTOM_PATH=".:$ROOT_DIR/bin"
+
 source $ROOT_DIR/system/environment.sh
 source $ROOT_DIR/system/bundles.sh
 source $ROOT_DIR/system/aliases.sh
 source $ROOT_DIR/system/functions.sh
-
-# Insert current directory into PATH variable
-export PATH=".:$ROOT_DIR/bin$PATH"
