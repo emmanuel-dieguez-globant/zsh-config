c984f059077a6561155b085f2fd1ec9f5209caca
edieguez <dieguezemmanuel@gmail.com>
2021-01-03 00:10:36 -0600

    Changed bundles loading to fix FZF issue

--- system/system.sh
+++ system/system.sh
@@ -9,15 +9,14 @@ export ANTIBODY_HOME=$HOME/.antibody
 source <(antibody init)
 
 # Default and custom bundles
+source $ROOT_DIR/system/bundles.sh
+
+antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
 antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
 antibody bundle robbyrussell/oh-my-zsh path:plugins/fzf
 antibody bundle robbyrussell/oh-my-zsh path:plugins/git
 antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
 antibody bundle robbyrussell/oh-my-zsh path:plugins/z
-
-source $ROOT_DIR/system/bundles.sh
-
-antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
 
 # Set custom theme
 antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
