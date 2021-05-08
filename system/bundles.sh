bb7248cb1636e100a20cde816814f4bd26847afd
edieguez <dieguezemmanuel@gmail.com>
2021-05-08 11:14:06 -0500

    Moved default plugins to bundles.sh

--- system/bundles.sh
+++ system/bundles.sh
@@ -1,2 +1,11 @@
 # Load custom plugins
 antibody bundle edieguez/zsh-config path:plugins kind:fpath
+
+antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
+antibody bundle robbyrussell/oh-my-zsh path:plugins/command-not-found
+antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
+antibody bundle robbyrussell/oh-my-zsh path:plugins/fzf
+antibody bundle robbyrussell/oh-my-zsh path:plugins/git
+antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
+antibody bundle robbyrussell/oh-my-zsh path:plugins/z
+
