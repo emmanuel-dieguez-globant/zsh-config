2e198b3f732ab8195681d3295ce0bb80e4d2a51e
edieguez <dieguezemmanuel@gmail.com>
2022-03-09 22:19:45 -0600

    Added misc aliases

--- system/aliases.sh
+++ system/aliases.sh
@@ -1,5 +1,8 @@
-alias youtube-dl-mp3='youtube-dl --add-metadata --extract-audio --audio-format mp3'
+alias browse-installed-packages="yay -Qq | fzf --preview 'yay -Qil {}' --layout=reverse --bind 'enter:execute(yay -Qil {} | less)'"
+alias browse-known-packages="yay -Slq | fzf --preview 'yay -Si {}' --layout=reverse"
 alias dict='trans en:es -d'
+alias lg=lazygit
+alias youtube-dl-mp3='youtube-dl --add-metadata --extract-audio --audio-format mp3'
 
 # Load aliases for local environment
 source_if_exist "$ROOT_DIR/system/aliases_local.sh"
