a5183b7f60529ef6f07ccf51faaf22ce623b55a7
edieguez <dieguezemmanuel@gmail.com>
2022-03-19 12:28:44 -0600

    Add backup script for local configuration

---
+++ bin/local_backup.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+printf "[34m[i] Starting local backup\n[0m"
+initial_dir=$(pwd)
+
+printf "[34m[i] Moving to $ROOT_DIR\n[0m"
+pushd $ROOT_DIR > /dev/null
+
+printf "[34m[i] Searching for local configuration files\n[0m"
+local_files=$(find -L . -type d -name local -or -regex '.*_local.sh$')
+
+if [ -n "$local_files" ]; then
+    printf "[34m[i] Local configuration files found\n[0m"
+
+    backup_filename="$(date +%Y%m%d_%H%M%S) $(hostname).tar.gz"
+    printf "[01;34m[i] Creating backup file: $backup_filename\n[0m"
+
+    tar cvzf "$initial_dir/$backup_filename" $local_files
+
+    printf "[34m[i] Local backup finished\n[0m"
+    popd > /dev/null
+else
+    printf "[01;33m[!] No local configuration files found\n[0m"
+
+    popd > /dev/null
+    exit 1
+fi
