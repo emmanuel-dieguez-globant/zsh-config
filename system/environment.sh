aa56efbec093942086bd827718157381f8d1b9e5
edieguez <dieguezemmanuel@gmail.com>
2022-06-04 13:32:06 -0400

    Changes to integrate SDKman

--- system/environment.sh
+++ system/environment.sh
@@ -1,4 +1,13 @@
-# Set your custom environment variables here
-export JAVA_HOME="$HOME/.current_jdk"
+# Remove duplicates from history
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
 
-CUSTOM_PATH+=":$JAVA_HOME/bin"
+# CUSTOM_PATH customization
+# export JAVA_HOME="$HOME/.current_jdk"
+# export CUSTOM_PATH="$JAVA_HOME/bin"
+
+export SDKMAN_DIR="$HOME/.sdkman"
