ea84f7bf63fe7e5178debb6e9489ed0a60f2a5ec
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:07 -0600

    Added function for 0x0 upload

--- system/functions.sh
+++ system/functions.sh
@@ -1,3 +1,13 @@
+0x0() {
+    if [ $# -lt 1 ]; then
+        echo "Usage: 0x0 <file>"
+        return 1
+    fi
+
+    echo "Uploading $1 to 0x0.st. Please wait..."
+    curl -F "file=@$1" -Fsecret= https://0x0.st
+}
+
 docker() {
     systemctl status docker.service | grep 'active (running)' > /dev/null
     [ $? -ne 0 ] && sudo systemctl start docker.service
