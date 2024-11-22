5734acb01eaff9ef8654d21ab427bbd131500e2c
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Small refactor to load custom plugins

--- system/bundles.sh
+++ system/bundles.sh
@@ -1,8 +1,5 @@
 # Load custom plugins
 antibody bundle edieguez/zsh-config path:plugins kind:fpath
 
-# Load oh-my-zsh
-antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
-
 # Set custom theme
 antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
