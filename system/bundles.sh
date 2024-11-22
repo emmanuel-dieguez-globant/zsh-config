b74a27e2a0ece5379f677fd78948f1d5bb977ef7
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Added syntax highlighting plugin

--- system/bundles.sh
+++ system/bundles.sh
@@ -15,3 +15,7 @@ export plugins=(
 source_if_exist "$ROOT_DIR/system/bundles_local.sh"
 
 antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
+
+# Must be sourced at the end
+# https://github.com/zsh-users/zsh-syntax-highlighting
+antibody bundle zsh-users/zsh-syntax-highlighting path:zsh-syntax-highlighting.plugin.zsh
