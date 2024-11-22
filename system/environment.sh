91b1c2fb610e3ce9a074c9c7391118e1833f9ea6
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Fixed bug with CUSTOM_PATH variable

--- system/environment.sh
+++ system/environment.sh
@@ -6,7 +6,7 @@ setopt HIST_IGNORE_SPACE
 setopt HIST_FIND_NO_DUPS
 setopt HIST_SAVE_NO_DUPS
 
-# CUSTOM_PATH customization
+# PATH customization
 # export JAVA_HOME="$HOME/.current_jdk"
 # export CUSTOM_PATH="$JAVA_HOME/bin"
 
