81db10413b8392980169b0cc57dd1cc91c9ed31b
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:07 -0600

    Update script to update repositories

--- bin/update-repos.sh
+++ bin/update-repos.sh
@@ -10,7 +10,19 @@ echo "Recursively updating repos in $target"
 fd -t directory --hidden --no-ignore-vcs '^.git$' $target | while read -r repo; do
 	repo=$(dirname "$repo")
 	pushd "$repo" 2>&1 >/dev/null
-	echo -e "\nPulling changes in $repo"
+
+	# array for remotes
+	remotes=(
+		$(git remote -v | awk '{print $1,$2}' | uniq)
+	)
+
+	if [ ${#remotes[@]} -eq 0 ]; then
+		echo "No remotes found in $repo"
+		continue
+	fi
+
+	echo -e "\nFetching changes in $repo"
+	git fetch --all --prune
 
 	git pull --rebase
 
