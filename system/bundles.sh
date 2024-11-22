9c3e0cf3d1d88a7bee8992a04a7511d933cb7a03
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Changes to integrate SDKman

--- system/bundles.sh
+++ system/bundles.sh
@@ -16,6 +16,8 @@ source_if_exist "$ROOT_DIR/system/bundles_local.sh"
 
 antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
 
-# Must be sourced at the end
 # https://github.com/zsh-users/zsh-syntax-highlighting
 antibody bundle zsh-users/zsh-syntax-highlighting path:zsh-syntax-highlighting.plugin.zsh
+
+# https://sdkman.io/install
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- system/system.sh
+++ system/system.sh
@@ -4,33 +4,24 @@ source_if_exist() {
     fi
 }
 
-# Custom enviroment and path modifications
 export ROOT_DIR="$HOME/.zsh-config"
-export CUSTOM_PATH=".:$ROOT_DIR/bin:$ROOT_DIR/bin/local"
 
-# Local path has priority over default custom path
 source_if_exist "$ROOT_DIR/system/environment_local.sh"
 source "$ROOT_DIR/system/environment.sh"
 
-PATH="$CUSTOM_PATH:$PATH"
-
-# Remove duplicates from history
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_FIND_NO_DUPS
-setopt HIST_SAVE_NO_DUPS
-
-# Antibody official site
 # https://getantibody.github.io
 export ANTIBODY_HOME=$HOME/.antibody
-source <(antibody init)
+source <($ROOT_DIR/bin/antibody init)
 
-# Source default configuration
 source "$ROOT_DIR/system/bundles.sh"
 source "$ROOT_DIR/system/aliases.sh"
 source "$ROOT_DIR/system/functions.sh"
+
+if [ -n "$CUSTOM_PATH" ]; then
+    PATH="$CUSTOM_PATH:$PATH"
+fi
+
+PATH=".:$ROOT_DIR/bin:$ROOT_DIR/bin/local:$PATH"
 
 # Set custom theme
 antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
