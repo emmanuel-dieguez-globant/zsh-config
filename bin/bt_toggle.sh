50f5fa758ff2ee2c4f2d5844c95a8f17ff79981e
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Added script to turn on/off bluetooth

---
+++ bin/bt_toggle.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+# Simple script to turn on/off bluetooth
+# returns:
+#   0 - bluetooth turned off
+#   1 - bluetooth turned on
+#   2 - unexpected status
+
+set -e
+
+bt_status=$(rfkill | grep bluetooth | awk '{print $4}')
+
+if [ $bt_status = 'blocked' ] || [ $bt_status = 'unblocked' ]; then
+    if [ $bt_status == 'blocked' ]; then
+        rfkill unblock bluetooth
+        exit 1
+    else
+        rfkill block bluetooth
+        exit 0
+    fi
+else
+    exit 1
+fi
+
