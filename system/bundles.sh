f6153bba0348d4156e6b027c8a764c9a7ac47665
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Added oh-my-zsh completion

--- system/bundles.sh
+++ system/bundles.sh
@@ -4,4 +4,8 @@ antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
 antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
 antibody bundle robbyrussell/oh-my-zsh path:plugins/z
 
+# Load oh-my-zsh completions
+antibody bundle robbyrussell/oh-my-zsh path:lib/completion.zsh
+
+# Set custom theme
 antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
