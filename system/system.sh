215b26fd2e9eef075cd5954e36879006ed470a15
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Added directory for ignored binaries

--- system/system.sh
+++ system/system.sh
@@ -4,7 +4,7 @@
 export ROOT_DIR="$HOME/.zsh-config"
 
 # Insert current directory into PATH variable
-export CUSTOM_PATH=".:$ROOT_DIR/bin"
+export CUSTOM_PATH=".:$ROOT_DIR/bin:$ROOT_DIR/bin/platform"
 
 source $ROOT_DIR/system/environment.sh
 source $ROOT_DIR/system/bundles.sh
