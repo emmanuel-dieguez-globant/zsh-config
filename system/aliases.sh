979939eb0cc585247bc66235a6372553f88182c3
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:07 -0600

    Added alias to see Git history as graph

--- system/aliases.sh
+++ system/aliases.sh
@@ -2,6 +2,7 @@ alias aria2c='aria2c --file-allocation=none --summary-interval=900'
 alias browse-installed-packages="yay -Qq | fzf --preview 'yay -Qil {}' --layout=reverse --bind 'enter:execute(yay -Qil {} | less)'"
 alias browse-known-packages="yay -Slq | fzf --preview 'yay -Si {}' --layout=reverse"
 alias dict='trans en:es -d'
+alias gitgraph="git log --graph --format='format:%C(yellow)%h%C(reset) %s %C(magenta)%cr%C(reset)%C(auto)%d%C(reset)'"
 alias ipv4='curl --silent -4 icanhazip.com'
 alias ipv6='curl --silent -6 icanhazip.com'
 alias leech='aria2c --seed-time=0'
