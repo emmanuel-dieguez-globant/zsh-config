feb857d7a0d884f5776f22a5a3828698c04f0551
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Refactor in installation script

--- system/system.sh
+++ system/system.sh
@@ -1,12 +1,20 @@
-#-------------------------------------------------------------------------------
-# Source functions
-#-------------------------------------------------------------------------------
+# Custom enviroment and path modifications
 export ROOT_DIR="$HOME/.zsh-config"
-
-# Insert current directory into PATH variable
 export CUSTOM_PATH=".:$ROOT_DIR/bin:$ROOT_DIR/bin/platform"
-
 source $ROOT_DIR/system/environment.sh
+
+export ANTIBODY_HOME=$HOME/.antibody
+source <(antibody init)
+
+# Default and custom bundles
+antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
+antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
+antibody bundle robbyrussell/oh-my-zsh path:plugins/z
+
 source $ROOT_DIR/system/bundles.sh
+
+# Default and custom aliases
 source $ROOT_DIR/system/aliases.sh
+
+# Default and custom functions
 source $ROOT_DIR/system/functions.sh
