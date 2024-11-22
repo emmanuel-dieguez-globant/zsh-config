2a6de3e5a049d9ed655fa1c0282dcb222da58692
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Added conditional load of custom scripts

--- system/bundles.sh
+++ system/bundles.sh
@@ -1,21 +1,2 @@
-# Load ZSH plugins
-export plugins=(
-    command-not-found
-    extract
-    fzf
-    git
-    sudo
-    virtualenv_py
-    z
-)
-
-# Load bundles for local environment
-source_if_exist "$ROOT_DIR/system/bundles_local.sh"
-
-antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
-
-# https://github.com/zsh-users/zsh-syntax-highlighting
-antibody bundle zsh-users/zsh-syntax-highlighting path:zsh-syntax-highlighting.plugin.zsh
-
 # https://sdkman.io/install
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
