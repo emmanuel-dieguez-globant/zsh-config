fd2fcb4f3a10bc21bb4b89d83ebcbb2dffcf4580
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Moved default plugins to bundles.sh

--- system/system.sh
+++ system/system.sh
@@ -11,14 +11,6 @@ source <(antibody init)
 # Default and custom bundles
 source $ROOT_DIR/system/bundles.sh
 
-antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
-antibody bundle robbyrussell/oh-my-zsh path:plugins/command-not-found
-antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
-antibody bundle robbyrussell/oh-my-zsh path:plugins/fzf
-antibody bundle robbyrussell/oh-my-zsh path:plugins/git
-antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
-antibody bundle robbyrussell/oh-my-zsh path:plugins/z
-
 # Set custom theme
 antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
 
@@ -27,3 +19,4 @@ source $ROOT_DIR/system/aliases.sh
 
 # Default and custom functions
 source $ROOT_DIR/system/functions.sh
+
