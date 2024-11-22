3631c8c5435ee2be3130695166b8d614e8d507c0
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:07 -0600

    Add bt-toggle Mac support

--- bin/bt-toggle.sh
+++ bin/bt-toggle.sh
@@ -8,15 +8,44 @@
 
 set -e
 
-bt_status=$(rfkill list bluetooth | grep -i 'soft' | awk '{print $3}')
+rfkill_toggle() {
+    # Linux systems
+    bt_status=$(rfkill list bluetooth | grep -i 'soft' | awk '{print $3}')
 
-if [ "$bt_status" = "yes" -o "$bt_status" = "no" ]; then
     if [ "$bt_status" == "yes" ]; then
+        # Turn on bluetooth
         rfkill unblock bluetooth
-    else
+    elif [ "$bt_status" == "no" ]; then
+        # Turn off bluetooth
         rfkill block bluetooth
         exit 0
     fi
-fi
 
-exit 1
+    exit 1
+}
+
+blueutil_toggle() {
+    # MacOS systems
+    bt_status=$(blueutil --power)
+
+    if [ "$bt_status" = 0 ]; then
+        # Turn on bluetooth
+        blueutil --power 1
+        exit 0
+    elif [ "$bt_status" = 1 ]; then
+        # Turn off bluetooth
+        blueutil --power 0
+        exit 0
+    fi
+
+    exit 1
+}
+
+if type -p rfkill > /dev/null; then
+    rfkill_toggle
+elif type -p blueutil > /dev/null; then
+    blueutil_toggle
+else
+    echo "rfkill or blueutil not found"
+    exit 1
+fi
