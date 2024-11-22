5355a3a65f3fe3920b716b50889450e1ae1da47c
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Updated zshrc and installation script

--- system/install.sh
+++ system/install.sh
@@ -1,17 +1,22 @@
 #! /usr/bin/env zsh
-export ANTIBODY_HOME=$HOME/.antibody
-mkdir -vp $HOME/.antibody
+export ZSH_HOME="$HOME/.zsh"
 
-curl -sfL git.io/antibody | sh -s - -b $ANTIBODY_HOME
+pushd "$ZSH_HOME"
 
-echo 'Installing basic configuration'
-$ANTIBODY_HOME/antibody bundle edieguez/zsh-config path:themes/aya.zsh-theme branch:antibody-migration
+echo 'Cloning required repositories'
+git clone git@github.com:ohmyzsh/ohmyzsh.git
+git@github.com:edieguez/zsh-config.git
 
-echo 'Creating symbolic links'
-ln -vsfn $ANTIBODY_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-edieguez-SLASH-zsh-config $HOME/.zsh-config
+
+export OH_MY_ZSH="$ZSH_HOME/ohmyzsh"
+export ZSH_CUSTOM="$ZSH_HOME/zsh-config"
+
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+popd
+
+echo 'Creating symbolic link for .zshrc'
 ln -vsfn $HOME/.zsh-config/system/zshrc $HOME/.zshrc
-
-echo 'Moving Antobody binary to new path'
-mv -v $ANTIBODY_HOME/antibody $HOME/.zsh-config/bin
 
 echo 'Done. Restart your shell to finish the setup'
