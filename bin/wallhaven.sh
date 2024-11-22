cf2a4191195079e7010b92a6b836552f271230a9
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Improved messages for wallhaven.sh

--- bin/wallhaven.sh
+++ bin/wallhaven.sh
@@ -2,10 +2,11 @@
 # https://wallhaven.cc/help/api
 
 if [ -z "$WALLHAVEN_KEY" ]; then
-  echo 'WALLHAVEN_KEY is not set'
-  echo 'Go to https://wallhaven.cc/settings/account and get your API key'
-  exit 1
+	echo '[01;33m[w] WALLHAVEN_KEY token is not set[0m'
+	echo '[01;34m[i] Go to https://wallhaven.cc/settings/account and get your API key[0m'
 fi
+
+echo '[01;34m[i] API reference: https://wallhaven.cc/help/api[0m'
 
 API_URL="https://wallhaven.cc/api/v1/search?apikey=$WALLHAVEN_KEY"
 : ${WALLHAVEN_DIR:="$HOME/Pictures/wallhaven"}
@@ -23,14 +24,14 @@ SEED=$RANDOM
 links=$(curl --silent "$API_URL&q=${Q// /%20}&categories=$CATEGORIES&purity=$PURITY&sorting=$SORTING&atleast=$ATLEAST&ratios=$RATIOS&seed=$SEED" | jq -r '.data[].path')
 
 if [ -z "$links" ]; then
-  echo "No wallpapers found for query: $Q"
-  exit 1
+	echo "[01;33m[w] No wallpapers found for query: $Q[0m"
+	exit 1
 fi
 
 if [ -d "$WALLHAVEN_DIR" ]; then
-  rm -r "$WALLHAVEN_DIR"
+	rm -r "$WALLHAVEN_DIR"
 fi
 
-echo "Found $(echo "$links" | wc -l) wallpapers"
+echo "[01;34m[i] Found $(echo "$links" | wc -l) wallpapers[0m"
 
 wget --quiet --show-progress --directory-prefix "$WALLHAVEN_DIR" -i <(echo "$links")
