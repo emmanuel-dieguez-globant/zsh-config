e8759f1d520e8d7319febb2a7cd1a457fb1dc8a5
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Moved local scripts into general scripts

--- system/bundles.sh
+++ system/bundles.sh
@@ -1,10 +1,17 @@
 # Load custom plugins
 antibody bundle edieguez/zsh-config path:plugins kind:fpath
 
+# Load ZSH plugins
+export plugins=(
+    command-not-found
+    extract
+    fzf
+    git
+    sudo
+    z
+)
+
+# Load bundles for local environment
+source_if_exist "$ROOT_DIR/system/bundles_local.sh"
+
 antibody bundle robbyrussell/oh-my-zsh path:oh-my-zsh.sh
-antibody bundle robbyrussell/oh-my-zsh path:plugins/command-not-found
-antibody bundle robbyrussell/oh-my-zsh path:plugins/extract
-antibody bundle robbyrussell/oh-my-zsh path:plugins/fzf
-antibody bundle robbyrussell/oh-my-zsh path:plugins/git
-antibody bundle robbyrussell/oh-my-zsh path:plugins/sudo
-antibody bundle robbyrussell/oh-my-zsh path:plugins/z
