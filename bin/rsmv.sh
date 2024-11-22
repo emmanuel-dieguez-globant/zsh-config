47fa72fd486300171fd93711d3a0708dfdcb8436
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:06 -0600

    Scripts to copy and move using rsync

---
+++ bin/rsmv.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file> <file> ... <destination>"
+    exit 1
+fi
+
+if [ -d "${@: -1}" ]; then
+    set -- "${@:1:$#-1}" "${@: -1}/"
+fi
+
+rsync -avP \
+    --compress \
+    --recursive --links --perms --times --group --owner --devices --specials \
+    --human-readable \
+    --remove-source-files "$@" && rm -rf "${@:1:$#-1}"
