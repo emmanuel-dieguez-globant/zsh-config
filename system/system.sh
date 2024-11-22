3fa39eff11027afb6e7999ac3d41269c82b37859
edieguez <dieguezemmanuel@gmail.com>
2024-11-21 22:05:05 -0600

    Fixed wrong path in system.sh

--- system/system.sh
+++ system/system.sh
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Source functions
 #-------------------------------------------------------------------------------
-local ROOT_DIR="$HOME/.antigen/bundles/edieguez/zsh-config"
+local ROOT_DIR="$HOME/.antigen/bundles/emmanuel-dieguez-globant/zsh-config"
 PATH="$ROOT_DIR/bin:$PATH"
 
 source $ROOT_DIR/system/environment.sh
